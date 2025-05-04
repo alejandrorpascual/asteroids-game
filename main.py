ea8e3add983e91b96cf9cae3e5e4f84831cdbc2b
alejandrorpascual <alexandros2707@gmail.com>
2025-05-04 11:49:57 -0400

    draws player into screen

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 import constants
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     dt = 0
 
     print("Starting Asteroids!")
@@ -18,6 +20,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 100
 
